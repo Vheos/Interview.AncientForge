7634a975dec12ba8423473bd53161799ca54c1a9
Vheos <Vheos777@gmail.com>
2023-08-23 01:57:46 +0200

    Added unit visuals

---
+++ Assets/Scripts/Combat/Visuals.cs
@@ -0,0 +1,25 @@
+﻿namespace AFSInterview.Combat
+{
+	using DG.Tweening;
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using UnityEngine;
+	using Vheos.Helpers.UnityObjects;
+
+	[RequireComponent(typeof(Renderer))]
+	public class UnitVisuals : MonoBehaviour
+	{
+		internal void ConnectTo(Unit unit)
+		{
+			this.BecomeChildOf(unit);
+			unit.OnTeamChanged += UpdateColor;
+		}
+
+		private void UpdateColor(Team previous, Team current)
+		{
+			Color color = current != null ? current.Color : Color.white;
+			GetComponent<Renderer>().material.DOColor(color, 1f);
+		}
+	}
+}
