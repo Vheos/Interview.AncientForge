adb07161c87da08ed5db9647164e0cf439a172fc
Vheos <Vheos777@gmail.com>
2023-08-23 08:47:19 +0200

    Added defaults for serialized fields

--- Assets/Scripts/Combat/UnitDefinition.cs
+++ Assets/Scripts/Combat/UnitDefinition.cs
@@ -4,6 +4,7 @@ namespace AFSInterview.Combat
 	using System.Collections.Generic;
 	using System.Linq;
 	using UnityEngine;
+	using Vheos.Helpers.Math;
 
 	[CreateAssetMenu(fileName = nameof(UnitDefinition), menuName = ASSET_MENU_PATH + nameof(UnitDefinition))]
 	public class UnitDefinition : ScriptableObject
@@ -11,13 +12,13 @@ namespace AFSInterview.Combat
 		public const string ASSET_MENU_PATH = nameof(Combat) + "/";
 
 		[Header("Defense")]
-		[SerializeField, Range(1, 20)] private int health;
-		[SerializeField, Range(0, 10)] private int armor;
-		[SerializeField] private UnitAttribute[] attributes;
+		[SerializeField, Range(1, 30)] private int health = 10;
+		[SerializeField, Range(0, 5)] private int armor = 0;
+		[SerializeField] private UnitAttribute[] attributes = Array.Empty<UnitAttribute>();
 		[Header("Offense")]
-		[SerializeField, Range(1, 10)] private int interval;
-		[SerializeField, Range(1, 10)] private int damage;
-		[SerializeField] private DamageModifier[] damageModifiers;
+		[SerializeField, Range(1, 10)] private int cooldown = 1;
+		[SerializeField, Range(1, 10)] private int damage = 1;
+		[SerializeField] private DamageModifier[] damageModifiers = Array.Empty<DamageModifier>();
 
 		public int Health => health;
 		public int Armor => armor;
