2bc5d46996fff140919d00f556ff6c19ff7bf5c2
Vheos <Vheos777@gmail.com>
2023-08-23 08:48:47 +0200

    Added `AddUnit()`, `RemoveUnit()` and `IsAnyAlive`

--- Assets/Scripts/Combat/Team.cs
+++ Assets/Scripts/Combat/Team.cs
@@ -1,9 +1,9 @@
 namespace AFSInterview.Combat
 {
-	using System;
 	using System.Collections.Generic;
 	using System.Linq;
 	using UnityEngine;
+	using Vheos.Helpers.Collections;
 
 	[CreateAssetMenu(fileName = nameof(Team), menuName = ASSET_MENU_PATH + nameof(Team))]
 	public class Team : ScriptableObject
@@ -15,5 +15,11 @@ namespace AFSInterview.Combat
 
 		public Color Color => color;
 		public IReadOnlyCollection<Unit> Units => units;
+		public bool IsAnyAlive => units.Any(unit => unit.IsAlive);
+
+		public void AddUnit(Unit unit)
+			=> units.Add(unit);
+		public void RemoveUnit(Unit unit)
+			=> units.Remove(unit);
 	}
 }
