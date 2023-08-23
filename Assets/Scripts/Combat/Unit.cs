9ff11f89e3a63ca2c8d4d085920c4dd640dc2184
Vheos <Vheos777@gmail.com>
2023-08-23 08:42:30 +0200

    Added unit animations

--- Assets/Scripts/Combat/Unit.cs
+++ Assets/Scripts/Combat/Unit.cs
@@ -30,6 +30,21 @@ namespace AFSInterview.Combat
 				OnTeamChanged(previous, team);
 			}
 		}
+		public void AnimateLookAt(Unit target)
+			=> transform.DOLookAt(target.transform.position, 0.25f);
+		public void AnimateAttack(float strength)
+			=> transform.DOPunchPosition(transform.localRotation.Far() * 0.5f.Lerp(1.5f, strength), 0.5f, 0, 0);
+		public void AnimateTakeDamage(float strength)
+		{
+			transform.DOShakeScale(0.5f, 0.25f.Lerp(0.5f, strength));
+			transform.DOShakeRotation(0.5f, 15.Lerp(30, strength));
+			visuals.Material.color = Color.white;
+			visuals.Material.DOColor(team.Color, 1.Lerp(2, strength));
+		}
+		public void AnimateDeath(bool destroyGameObject)
+			=> transform.DOScale(0f, 2f).OnComplete(() => gameObject.Destroy());
+		public void AnimateCooldown()
+			=> transform.DOPunchScale(Vector3.down / 2f, 1f, 0, 0);
 
 		private void Awake()
 		{
